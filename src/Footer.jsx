2162863e4322310f4e05af76abf3d0aff9b83d74
Priyank-Aggarwal35 <priyankaggarwal35@gmail.com>
2024-04-12 10:18:27 +0530

    Make Navbar and create a Routing By Priyank Aggarwal

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,24 +1,24 @@
-function Footer(){
-return (
-    <>
-    <footer>
-    <div class="footer-content">
-        <p><center>Welcome to Vertigo, the ultimate online extravaganza brought to you by Technovation ! Get ready to immerse yourself in a whirlwind of excitement and gaming brilliance.</center> </p>
-    </div>
-    <div class="footer-social" style={{marginTop:'10px', marginRight:'10px'}}>
-        <ul>
-            <li><a href="#"><i class="fa fa-facebook"  style={{ fontSize: '20px', color: 'red', marginLeft:'10px' }}></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram" style={{ fontSize: '20px', color: 'red' ,marginLeft:'10px'}}></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin" style={{ fontSize: '20px', color: 'red' ,marginLeft:'10px'}}></i></a></li>
-            <li><a href="#"><i class="fa fa-address-book"  style={{ fontSize: '20px', color: 'red',marginLeft:'10px' }}></i></a></li>
-        </ul>
-    </div>
-    <div class="footer-bottom">
-        <p>&copy; 2024 Vertigo. All rights reserved.</p>
-    </div>
-</footer>
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="footer-content">
+                    <p><center>Welcome to Vertigo, the ultimate online extravaganza brought to you by Technovation ! Get ready to immerse yourself in a whirlwind of excitement and gaming brilliance.</center> </p>
+                </div>
+                <div className="footer-social" style={{ marginTop: '10px', marginRight: '10px' }}>
+                    <ul>
+                        <li><a href="#"><i className="fa fa-facebook" style={{ fontSize: '20px', color: 'red', marginLeft: '10px' }}></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram" style={{ fontSize: '20px', color: 'red', marginLeft: '10px' }}></i></a></li>
+                        <li><a href="#"><i className="fa fa-linkedin" style={{ fontSize: '20px', color: 'red', marginLeft: '10px' }}></i></a></li>
+                        <li><a href="#"><i className="fa fa-address-book" style={{ fontSize: '20px', color: 'red', marginLeft: '10px' }}></i></a></li>
+                    </ul>
+                </div>
+                <div className="footer-bottom">
+                    <p>&copy; 2024 Vertigo. All rights reserved.</p>
+                </div>
+            </footer>
 
-    </>
-);
+        </>
+    );
 }
 export default Footer;
